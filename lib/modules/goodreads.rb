20d5422236ec55e979b59e6bf4b64690c88a3b34
Alex Skryl <rut216@gmail.com>
2012-10-26 01:41:35 -0500

    auto pull note URLs from goodreads

--- lib/modules/goodreads.rb
+++ lib/modules/goodreads.rb
@@ -4,17 +4,20 @@ require 'xml_helper'
 class Goodreads < ModuleBase
   include XmlHelper
   READ_AT = "2005-01-01"
+  EVERNOTE_BASE = "https://www.evernote.com/shard/s6/sh"
 
   def update
     num_updates = 0
     uri = "http://www.goodreads.com/review/list/#{config.user_id}.xml?v=2&per_page=200&shelf=read&sort=date_read&key=#{config.key}"
     xml_for(uri, '//reviews/review') do |r|
+      note_path = r.find('recommended_for').first.content
       book = Book.new :goodreads_id => r.find('book/id').first.content, 
         :isbn13      => r.find('book/isbn13').first.content, 
         :title       => r.find('book/title').first.content.strip, 
         :finished_at => Time.parse(r.find('read_at').first.content),
         :num_pages   => r.find('book/num_pages').first.content, 
-        :rating      => r.find('rating').first.content
+        :rating      => r.find('rating').first.content,
+        :notes_url   => note_path.blank? ? nil : "#{EVERNOTE_BASE}/#{note_path}"
       r.find('book/authors/author').each do |a|
         book.add_author(a.find('name').first.content.strip)
       end
