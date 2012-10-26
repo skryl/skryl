0b4a7800313dad030ba602038969a64e4085b5fc
Alex Skryl <rut216@gmail.com>
2012-10-25 20:04:10 -0500

    readding app config

---
+++ config/app.rb
@@ -0,0 +1,40 @@
+require 'app_extensions'
+require 'heroku_env'
+
+class App < Configurable # :nodoc:
+  extend AppExtensions
+
+  # Settings in config/app/* take precedence over those specified here.
+  config.name = Rails.application.class.parent.name
+
+  # Personal information
+  config.full_name    = 'Aleksey Skryl'
+  config.display_name = 'Aleksey Skryl'
+
+  for_module(:blog) do |mod|
+    mod.atom = ['http://thoughts.skryl.org/rss', 'http://origin.skryl.org/rss']
+  end
+
+  for_module(:github) do |mod|
+    mod.user = 'skryl'
+  end
+
+  for_module(:goodreads) do |mod|
+    mod.key     = ENV['GOODREADS_KEY']
+    mod.shelf   = 'read'
+    mod.user_id = ENV['GOODREADS_ID']
+  end
+
+  for_module(:twitter) do |mod|
+    mod.user = '_skryl_'
+  end
+
+  for_module(:zeo) do |mod|
+    MyZeo.headers 'Referer' => 'http://skryl.org'
+    mod.zeo_client = MyZeo.new(ENV['ZEO_KEY'], :login => ENV['ZEO_LOGIN'], :password => ENV['ZEO_PASSWORD'])
+  end
+
+  for_module(:nike_plus) do |mod|
+    mod.nike_client = Nike::Client.new(ENV['NIKE_LOGIN'], ENV['NIKE_PASSWORD'])
+  end
+end
